e1f86d5cce33c892b4ad921ffd4543c33c710e73
VickyUA <vickyy379@gmail.com>
2023-10-17 14:46:13 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const inputForm = document.querySelector('input');
+const textareaForm = document.querySelector('textarea');
+
+const LS_key = "feedback-form-state";
+    
+let formInfo = {};
+
+restoreForm();
+
+feedbackForm.addEventListener('input', throttle(handlerInput, 500));
+feedbackForm.addEventListener('submit', handlerSubmit);
+
+function handlerInput(event) {
+    formInfo[event.target.name] = event.target.value;
+    localStorage.setItem(LS_key, JSON.stringify(formInfo));
+}
+
+function handlerSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(LS_key);
+    formInfo = {};
+}
+
+function restoreForm() {
+    const savedData = JSON.parse(localStorage.getItem(LS_key));
+    if (savedData) {
+        const { email, message } = savedData;
+        if (email) {            
+            inputForm.value = email;
+            formInfo.email = email;     
+        
+        }
+        if (message) {
+            textareaForm.value = message;
+            formInfo.message = message;  
+        }
+    }    
+}
